ce04315d5f40c073cc702ca7af22e0aa7ef4d212
Joshua Send <joshuasend1@gmail.com>
2019-03-08 11:34:15 +0000

    Add deploy test user/pass and spawn grakn and use pip

--- tests/deployment/test.py
+++ tests/deployment/test.py
@@ -1,4 +1,18 @@
+from unittest import TestCase
 import grakn
 
 
 # TODO some tests
+
+
+class PythonApplicationTest(TestCase):
+
+    def test_define_schema(self):
+        pass
+
+    def test_match_query(self):
+        pass
+
+    def test_insert_query(self):
+        pass
+
