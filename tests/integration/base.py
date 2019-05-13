8364cb35aab4f1cdb47cab89bd6bd2f510a22441
Joshua Send <joshuasend1@gmail.com>
2019-05-13 12:20:52 +0100

    revert debuggnig modification

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -62,19 +62,17 @@ class GraknServer(object):
         self.__unpacked_dir = None
 
     def __enter__(self):
-        # if not self.__unpacked_dir:
-        #     self._unpack()
-        # sp.check_call([
-        #     'grakn', 'server', 'start'
-        # ], cwd=os.path.join(self.__unpacked_dir, GraknServer.DISTRIBUTION_ROOT_DIR))
-        pass
+        if not self.__unpacked_dir:
+            self._unpack()
+        sp.check_call([
+            'grakn', 'server', 'start'
+        ], cwd=os.path.join(self.__unpacked_dir, GraknServer.DISTRIBUTION_ROOT_DIR))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # sp.check_call([
-        #     'grakn', 'server', 'stop'
-        # ], cwd=os.path.join(self.__unpacked_dir, GraknServer.DISTRIBUTION_ROOT_DIR))
-        # shutil.rmtree(self.__unpacked_dir)
-        pass
+        sp.check_call([
+            'grakn', 'server', 'stop'
+        ], cwd=os.path.join(self.__unpacked_dir, GraknServer.DISTRIBUTION_ROOT_DIR))
+        shutil.rmtree(self.__unpacked_dir)
 
     def _unpack(self):
         self.__unpacked_dir = tempfile.mkdtemp(prefix='grakn')
