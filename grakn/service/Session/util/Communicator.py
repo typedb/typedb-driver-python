7777613064ebf9aeabb9c5e63cce6b5d5bbbb86d
Adam Mitchell <adam@grakn.ai>
2020-06-23 12:34:00 +0100

    Fix leaking threads

--- grakn/service/Session/util/Communicator.py
+++ grakn/service/Session/util/Communicator.py
@@ -174,11 +174,11 @@ class Communicator(six.Iterator):
                     current._buffer_response(response)
 
             except Exception as e:
-                self._closed = True
                 if not current:
-                    raise GraknError("Internal client/protocol error, request/response pair not matched: {0}\n\n "
+                    self._error = GraknError("Internal client/protocol error, request/response pair not matched: {0}\n\n "
                                      "Ensure client version is compatible with server version.".format(e))
-                raise GraknError("Server/network error: {0}\n\n generated from request: {1}".format(e, current._request))
+                self._error = GraknError("Server/network error: {0}\n\n generated from request: {1}".format(e, current._request))
+                raise self._error
 
     def iteration_request(self, request, is_last_response):
         self.error_if_closed()
@@ -190,16 +190,23 @@ class Communicator(six.Iterator):
 
     def close(self):
         if not self._closed:
-
-            # Exhaust all resolvers, ensuring that transaction is closed without error
-
-            for resolver in list(self._resolver_queue):
-                resolver._on_close()
+            raise_error = False
+            if not self._error:
+                # Exhaust all resolvers, ensuring that transaction is closed without error
+                try:
+                    for resolver in list(self._resolver_queue):
+                        resolver._on_close()
+                except GraknError as e:
+                    raise_error = True
+            self._closed = True
 
             with self._request_queue.mutex:  # probably don't even need the mutex
                 self._request_queue.queue.clear()
             self._request_queue.put(None)
-            self._closed = True
+
+            if raise_error:
+                raise self._error
+
             # force exhaust the iterator so `onCompleted()` is called on the server
             try:
                 next(self._response_iterator)
