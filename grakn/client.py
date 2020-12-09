bf9741238a0ca951d3c6f0343e05333c7ab2116d
Alex Walker <alex@grakn.ai>
2020-12-09 17:50:03 +0000

    Implement session pulse

--- grakn/client.py
+++ grakn/client.py
@@ -1,6 +1,10 @@
-from grakn.rpc.DatabaseManager import DatabaseManager
-from grakn.rpc.Session import Session
 import grpc
+import sched
+import time
+
+from grakn.options import GraknOptions
+from grakn.rpc.database_manager import DatabaseManager
+from grakn.rpc.session import Session
 
 
 class GraknClient(object):
@@ -9,9 +13,10 @@ class GraknClient(object):
     def __init__(self, address=DEFAULT_URI):
         self._channel = grpc.insecure_channel(address)
         self._databases = DatabaseManager(self._channel)
+        self._scheduler = sched.scheduler(time.time, time.sleep)
 
-    def session(self, database, type, options=None):
-        return Session(self._channel, database, type, options)
+    def session(self, database, session_type, options=GraknOptions()):
+        return Session(self, database, session_type, options)
 
     def databases(self):
         return self._databases
--- grakn/rpc/session.py
+++ grakn/rpc/session.py
@@ -2,27 +2,30 @@ from graknprotocol.protobuf.grakn_pb2_grpc import GraknStub
 import graknprotocol.protobuf.session_pb2 as session_proto
 import enum
 
-from grakn.rpc.Transaction import Transaction
+from grakn.options import GraknOptions
+from grakn.proto_builder import GraknProtoBuilder
+from grakn.rpc.transaction import Transaction
 
 
 class Session(object):
 
-    def __init__(self, channel, database, session_type, options=None):
-        self._channel = channel
+    def __init__(self, client, database, session_type, options=GraknOptions()):
+        self._channel = client._channel
+        self._scheduler = client._scheduler
         self._database = database
         self._session_type = session_type
-        self._grpc_stub = GraknStub(channel)
+        self._grpc_stub = GraknStub(self._channel)
 
-        openReq = session_proto.Session.Open.Req()
-        openReq.database = database
-        openReq.type = Session._session_type_proto(session_type)
-        # TODO openReq.options = None
+        open_req = session_proto.Session.Open.Req()
+        open_req.database = database
+        open_req.type = Session._session_type_proto(session_type)
+        open_req.options.CopyFrom(GraknProtoBuilder.options(options))
 
-        self._session_id = self._grpc_stub.session_open(openReq).session_id
+        self._session_id = self._grpc_stub.session_open(open_req).session_id
         self._is_open = True
-        # TODO transmit pulses at regular intervals
+        self._pulse = self._scheduler.enter(5, 1, self._transmit_pulse, ())
 
-    def transaction(self, transaction_type, options=None):
+    def transaction(self, transaction_type, options=GraknOptions()):
         return Transaction(self._channel, self._session_id, transaction_type, options)
 
     def session_type(self): return self._session_type
@@ -38,6 +41,15 @@ class Session(object):
 
     def database(self): return self._database
 
+    def _transmit_pulse(self):
+        if not self._is_open:
+            return
+        pulse_req = session_proto.Session.Pulse.Req()
+        pulse_req.session_id = self._session_id
+        is_alive = self._grpc_stub.session_pulse(pulse_req).is_alive
+        if is_alive:
+            self._pulse = self._scheduler.enter(5, 1, self._transmit_pulse, ())
+
     def __enter__(self):
         return self
 
@@ -50,8 +62,10 @@ class Session(object):
 
     @staticmethod
     def _session_type_proto(session_type):
-        if session_type == Session.Type.DATA: return session_proto.Session.Type.Value("DATA")
-        if session_type == Session.Type.SCHEMA: return session_proto.Session.Type.Value("SCHEMA")
+        if session_type == Session.Type.DATA:
+            return session_proto.Session.Type.Value("DATA")
+        if session_type == Session.Type.SCHEMA:
+            return session_proto.Session.Type.Value("SCHEMA")
 
     class Type(enum.Enum):
         DATA = 0
