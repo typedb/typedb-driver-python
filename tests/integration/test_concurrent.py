9d5aeb0307629bf4895547c815c3a712d6b98056
Alex Walker <alex@grakn.ai>
2021-03-30 12:19:21 +0100

    Add simple explanations test

--- tests/integration/test_concurrent.py
+++ tests/integration/test_concurrent.py
@@ -26,16 +26,19 @@ from grakn.client import *
 
 
 GRAKN = "grakn"
+SCHEMA = SessionType.SCHEMA
 DATA = SessionType.DATA
 WRITE = TransactionType.WRITE
+READ = TransactionType.READ
 
 
 class TestConcurrent(TestCase):
 
     def setUp(self):
         with Grakn.core_client(Grakn.DEFAULT_ADDRESS) as client:
-            if not client.databases().contains(GRAKN):
-                client.databases().create(GRAKN)
+            if client.databases().contains(GRAKN):
+                client.databases().get(GRAKN).delete()
+            client.databases().create(GRAKN)
 
     def open_tx(self, session: GraknSession, *args):
         tx = session.transaction(WRITE)
@@ -51,6 +54,51 @@ class TestConcurrent(TestCase):
             pool.map(partial(self.open_tx, session), results)
             pool.close()
             pool.join()
+
+    def test_explanations(self):
+        with Grakn.core_client(Grakn.DEFAULT_ADDRESS) as client:
+            with client.session(GRAKN, SCHEMA) as session, session.transaction(WRITE) as tx:
+                schema = """
+                define
+                person sub entity, owns name, plays friendship:friend, plays marriage:husband, plays marriage:wife;
+                name sub attribute, value string;
+                friendship sub relation, relates friend;
+                marriage sub relation, relates husband, relates wife;
+                rule marriage-is-friendship: when {
+                    $x isa person; $y isa person; (husband: $x, wife: $y) isa marriage;
+                } then {
+                    (friend: $x, friend: $y) isa friendship;
+                };
+                rule everyone-is-friends: when {
+                    $x isa person; $y isa person; not { $x is $y; };
+                } then {
+                    (friend: $x, friend: $y) isa friendship;
+                };
+                """
+                tx.query().define(schema)
+                tx.commit()
+
+            with client.session(GRAKN, DATA) as session, session.transaction(WRITE) as tx:
+                data = """
+                insert
+                $x isa person, has name "Zack";
+                $y isa person, has name "Yasmin";
+                (husband: $x, wife: $y) isa marriage;
+                """
+                tx.query().insert(data)
+                tx.commit()
+
+            opts = GraknOptions.core()
+            opts.explain = True
+            with client.session(GRAKN, DATA) as session, session.transaction(READ, opts) as tx:
+                answers = list(tx.query().match("match (friend: $p1, friend: $p2) isa friendship; $p1 has name $na;"))
+                assert len(answers[0].explainables().relations()) == 1
+                assert len(answers[1].explainables().relations()) == 1
+                explanations = list(tx.query().explain(next(iter(answers[0].explainables().relations().values()))))
+                assert len(explanations) == 3
+                explanations2 = list(tx.query().explain(next(iter(answers[1].explainables().relations().values()))))
+                assert len(explanations2) == 3
+                print([str(e) for e in explanations])
 
 
 if __name__ == "__main__":
