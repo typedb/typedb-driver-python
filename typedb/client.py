896448810e28cfbea83a60057d1eef54f899b1c0
Alex Walker <alex@grakn.ai>
2021-07-15 16:24:55 +0100

    Export entire API package from typedb.client

--- typedb/client.py
+++ typedb/client.py
@@ -18,17 +18,48 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-from typing import Union, Iterable
 
-from typedb.api.connection.client import TypeDBClient, TypeDBClusterClient
-from typedb.api.connection.credential import TypeDBCredential
 from typedb.connection.cluster.client import _ClusterClient
 from typedb.connection.core.client import _CoreClient
 
 # Repackaging these symbols allows them to be imported from "typedb.client"
-from typedb.api.connection.options import TypeDBOptions  # noqa # pylint: disable=unused-import
-from typedb.api.connection.session import TypeDBSession, SessionType  # noqa # pylint: disable=unused-import
-from typedb.api.connection.transaction import TypeDBTransaction, TransactionType  # noqa # pylint: disable=unused-import
+
+from typedb.api.answer.concept_map import *  # noqa # pylint: disable=unused-import
+from typedb.api.answer.concept_map_group import *  # noqa # pylint: disable=unused-import
+from typedb.api.answer.numeric import *  # noqa # pylint: disable=unused-import
+from typedb.api.answer.numeric_group import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.concept.thing.attribute import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.thing.entity import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.thing.relation import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.thing.thing import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.concept.type.attribute_type import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.type.entity_type import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.type.relation_type import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.type.role_type import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.type.thing_type import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.type.type import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.concept.concept import *  # noqa # pylint: disable=unused-import
+from typedb.api.concept.concept_manager import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.connection.client import *
+from typedb.api.connection.credential import *
+from typedb.api.connection.database import *  # noqa # pylint: disable=unused-import
+from typedb.api.connection.options import *  # noqa # pylint: disable=unused-import
+from typedb.api.connection.session import *  # noqa # pylint: disable=unused-import
+from typedb.api.connection.transaction import *  # noqa # pylint: disable=unused-import
+from typedb.api.connection.user import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.logic.explanation import *  # noqa # pylint: disable=unused-import
+from typedb.api.logic.logic_manager import *  # noqa # pylint: disable=unused-import
+from typedb.api.logic.rule import *  # noqa # pylint: disable=unused-import
+
+from typedb.api.query.query_manager import *  # noqa # pylint: disable=unused-import
+
+from typedb.common.exception import *  # noqa # pylint: disable=unused-import
+from typedb.common.label import *  # noqa # pylint: disable=unused-import
 
 
 class TypeDB:
