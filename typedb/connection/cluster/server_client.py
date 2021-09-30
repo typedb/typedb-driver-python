02f8fda831b2dc41b805e118aa98094cd0eb8ac7
Ganeshwara Hananda <ganesh@vaticle.com>
2021-09-30 16:10:57 +0100

    Authentication token - implementation

--- typedb/connection/cluster/server_client.py
+++ typedb/connection/cluster/server_client.py
@@ -46,7 +46,7 @@ class _ClusterServerClient(_TypeDBClientImpl):
 
     def new_channel_and_stub(self) -> (grpc.Channel, _ClusterServerStub):
         channel = self._new_channel()
-        return channel, _ClusterServerStub(channel)
+        return channel, _ClusterServerStub(channel, self._credential)
 
     def _new_channel(self) -> grpc.Channel:
         if self._credential.tls_root_ca_path() is not None:
