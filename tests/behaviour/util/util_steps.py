0a5e9cd1acd6299c19f5a69bc8f06dc38c40130b
joshua <joshuasend1@gmail.com>
2022-01-05 09:38:39 +0000

    create separate //tests/behaviour/util package

--- tests/behaviour/util/util_steps.py
+++ tests/behaviour/util/util_steps.py
@@ -19,17 +19,11 @@
 # under the License.
 #
 
+from time import sleep
 from behave import *
-
 from tests.behaviour.context import Context
 
 
-@step("connection has been opened")
-def step_impl(context: Context):
-    assert context.client and context.client.is_open()
-
-
-@step("connection does not have any database")
-def step_impl(context: Context):
-    assert len(context.client.databases().all()) == 0
-
+@step("wait {seconds} seconds")
+def step_impl(context: Context, seconds: str):
+    sleep(float(seconds))
