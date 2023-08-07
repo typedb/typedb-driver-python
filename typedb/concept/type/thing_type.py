05de1fb07262b4b855170203bfe0315960e63435
Dmitrii Mikhalin <17214250+dmikhalin@users.noreply.github.com>
2023-08-07 17:50:18 +0100

    Fix import

--- typedb/concept/type/thing_type.py
+++ typedb/concept/type/thing_type.py
@@ -28,6 +28,7 @@ from typedb.api.concept.type.thing_type import ThingType
 from typedb.common.label import Label
 from typedb.common.streamer import Streamer
 from typedb.common.transitivity import Transitivity
+from typedb.concept.concept_factory import attribute_type_of, role_type_of
 from typedb.concept.type.type import _Type
 from typedb.typedb_client_python import thing_type_is_root, thing_type_is_abstract, \
     thing_type_get_label, thing_type_delete, thing_type_is_deleted, thing_type_set_label, thing_type_set_abstract, \
@@ -108,7 +109,7 @@ class _ThingType(ThingType, _Type, ABC):
     def get_owns(self, transaction: _Transaction, value_type: Optional[ValueType] = None,
                  transitivity: Transitivity = Transitivity.TRANSITIVE, annotations: Optional[set[Annotation]] = None
                  ) -> Iterator[AttributeType]:
-        return map(type_.attribute_type._AttributeType,
+        return map(attribute_type_of,
                    Streamer(thing_type_get_owns(transaction.native_object,
                                                 self.native_object,
                                                 value_type.native_object if value_type else None,
@@ -122,7 +123,7 @@ class _ThingType(ThingType, _Type, ABC):
 
     def get_plays(self, transaction: _Transaction, transitivity: Transitivity = Transitivity.TRANSITIVE
                   ) -> Iterator[_RoleType]:
-        return map(type_.role_type._RoleType,
+        return map(role_type_of,
                    Streamer(thing_type_get_plays(transaction.native_object, self.native_object, transitivity.value),
                             concept_iterator_next))
 
@@ -130,17 +131,15 @@ class _ThingType(ThingType, _Type, ABC):
         return self.get_plays(transaction, Transitivity.EXPLICIT)
 
     def get_plays_overridden(self, transaction: _Transaction, role_type: _RoleType) -> Optional[_RoleType]:
-        from typedb.concept.type.role_type import _RoleType
-
         if res := thing_type_get_plays_overridden(transaction.native_object,
                                                   self.native_object, role_type.native_object):
-            return _RoleType(res)
+            return role_type_of(res)
         return None
 
     def get_owns_overridden(self, transaction: _Transaction, attribute_type: _AttributeType) -> Optional[AttributeType]:
         if res := thing_type_get_owns_overridden(transaction.native_object,
                                                  self.native_object, attribute_type.native_object):
-            return type_.attribute_type._AttributeType(res)
+            return attribute_type_of(res)
         return None
 
     def unset_owns(self, transaction: _Transaction, attribute_type: _AttributeType) -> None:
